d03d9917dcf58f1d4936846d9d1f65c30ba6d1bb
Diogo Duarte <dduarte@odoogap.com>
2022-08-22 12:28:11 +0100

    [IMP] initial commit

---
+++ website_sale_type/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Product Type on Webshop',
+    'version': '1.0',
+    'summary': 'Product Type on Webshop',
+    'sequence': 10,
+    'description': """
+Product Type on Webshop
+=======================
+    """,
+    'category': 'Website/Website',
+    'website': 'https://www.erpgap.com',
+    'depends': ['website_sale'],
+    'data': [
+        'views/templates.xml'
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
